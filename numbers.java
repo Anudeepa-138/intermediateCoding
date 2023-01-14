4f395dff55398cf3d4e996ef958449eababf11ae
Anudeepa-138 <72063495+Anudeepa-138@users.noreply.github.com>
2023-01-14 11:54:12 +0530

    Add files via upload

--- numbers.java
+++ numbers.java
@@ -1,7 +1,13 @@
-public class numbers
+import java.util.*;
+class numbers
 {
     public static void main(String[] args)
     {
-        
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i=n;i>=1;i--)
+        {
+            System.out.println(i);
+        }
     }
 }
